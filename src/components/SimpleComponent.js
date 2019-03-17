f7583943cbd4fd35b5c2a6a6136ed8a50898b1ae
JerYOMat <jeremy.emata@gmail.com>
2019-03-17 17:09:52 -0400

    updated with functional component that uses { useState } hook.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
-// Code SimpleComponent Here
+import React, { useState } from "react";
+
+
+function SimpleComponent() {
+  const [mood, setMood] = useState('happy');
+
+  /**
+   * return new mood based on previous mood
+   */
+  const calcMood = (prevMood) => {
+    const newMood = prevMood === 'happy'
+      ? 'sad'
+      : 'happy' ;
+    return newMood;
+  };
+
+  /**
+   * return function to set mood on click
+   */
+  const setMoodOnClick = () => setMood(calcMood(mood));
+  
+  return (
+    <div onClick={setMoodOnClick}>
+      {mood}
+    </div>
+  );
+}
+export default SimpleComponent; 
